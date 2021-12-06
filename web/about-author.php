19f4fdcc749e4e46d6c3145dad284fb4d27b2aa3
Fatkulin Andrei <anfatkulin@gmail.com>
2021-12-07 00:57:48 +0300

    init commit 3

---
+++ web/about-author.php
@@ -0,0 +1,2 @@
+<?php
+echo 'Начало страницы ABOUT-AUTHOR!';
--- web/index.php
+++ web/index.php
@@ -1,4 +1,15 @@
 <?php
+//echo "Hello HEROKU 2<br />";
+///echo 'I\'m alive!';
 
-echo "Hello HEROKU 2<br />";
-echo 'I\'m alive!';
+session_start();
+$content = [];
+
+if (!S_SESSION['user']) {
+    $content['msg'] = "<br>Вы не авторизованы <br>";
+    $content['form'] = true;
+} else {
+    $content['msg'] = "Пользователь: {$_SESSION['user']['name']}";
+    $content['form'] = false;
+}
+include_once 'about-author.php';
