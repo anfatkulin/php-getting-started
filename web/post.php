dc3f5aa54195be307ba4bd06ab4cb03b8a4169d1
Fatkulin Andrei <anfatkulin@gmail.com>
2021-12-07 01:04:00 +0300

    test

---
+++ web/post.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+
+if ($_SESSION['user']) {
+    $_SESSION['msg'] = 'Вы уже авторизованы';
+}
+if (!empty($_POST)) {
+    $_SESSION['user']['name'] = $_POST['name'];
+}
+
+header('Location: index.php');
+exit;
