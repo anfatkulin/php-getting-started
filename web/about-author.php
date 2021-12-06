5b0dcc7f6fe70a41fd4a6692c4ec1d11d361f2f0
Fatkulin Andrei <anfatkulin@gmail.com>
2021-12-07 01:14:03 +0300

    init commit 5

--- web/about-author.php
+++ web/about-author.php
@@ -1,2 +1,30 @@
 <?php
-echo 'Начало страницы ABOUT-AUTHOR!';
+//echo 'Начало страницы ABOUT-AUTHOR!';
+$form = (!$content['form']) ?
+    "<hr><a href=\"exit.php\">exit</a>"
+    : "<div>
+        <form action=\"post.php\" method=\"post\">
+            <label>
+                <span>Имя пользователя:</span>
+                <input required type=\"text\" name=\"name\" placeholder=\"Имя пользователя\">
+            </label>
+            <input type='submit'>
+            </form>
+        </div>\"";
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Index</title>
+</head>
+<body>
+
+<h3>Приветствие!</h3>
+
+<?= $content['msg'] ?>
+<?= $form ?>
+
+</body>
+</html>
